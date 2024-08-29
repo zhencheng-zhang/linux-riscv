57cb291a317e0c2d83af9ae566444ba3715fcee6
Xiaoguang Xing <xiaoguang.xing@sophgo.com>
2024-08-29 09:40:03 +0800

    driver: clk: Add Sophgo sg2044 and bm1690 clock support
    Signed-off-by: dong.yang <dong.yang@sophgo.com>

---
+++ include/dt-bindings/clock/sophgo-clk.h
@@ -0,0 +1,27 @@
+#ifndef __SOPHGO_CLK__
+#define __SOPHGO_CLK__
+
+//PLL ID
+#define MPLL0_CLK	0
+#define MPLL1_CLK	1
+#define MPLL2_CLK	2
+#define MPLL3_CLK	3
+#define MPLL4_CLK	4
+#define MPLL5_CLK	5
+#define FPLL0_CLK	6
+#define FPLL1_CLK	7
+#define DPLL0_CLK	8
+#define DPLL1_CLK	9
+#define DPLL2_CLK	10
+#define DPLL3_CLK	11
+#define DPLL4_CLK	12
+#define DPLL5_CLK	13
+#define DPLL6_CLK	14
+#define DPLL7_CLK	15
+
+//clock mode
+#define NORMAL_MODE	0
+#define FAST_MODE	1
+#define SAFE_MODE	2
+#define BYPASS_MODE	3
+#endif
